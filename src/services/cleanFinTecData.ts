fe990fa3d0c0e39db334ef0ccfd0492161b325c0
Ashish Sharma <90973051+Sharma4ashish@users.noreply.github.com>
2026-04-15 21:44:00 +0530

    added funcnalities for clear data only required fields

--- src/services/cleanFinTecData.ts
+++ src/services/cleanFinTecData.ts
@@ -5,16 +5,60 @@ export const cleanFinTecData = (data: any) => {
     
     const facts = data?.facts?.["us-gaap"];
     const companyName = data?.entityName;
-    const revenue = data?.facts?.["us-gaap"]?.ContractWithCustomerLiabilityRevenueRecognized?.units?.USD
-    const assets = data?.facts?.["us-gaap"]?.Assets?.units?.USD
-    const liabilities = data?.facts?.["us-gaap"]?.Liabilities?.units?.USD
+    // const revenue = data?.facts?.["us-gaap"]?.ContractWithCustomerLiabilityRevenueRecognized?.units?.USD
+    // const assets = data?.facts?.["us-gaap"]?.Assets?.units?.USD
+    // const liabilities = data?.facts?.["us-gaap"]?.Liabilities?.units?.USD
+
+    const cleanMetric = (metric: any) => {
+      if (!metric) return [];
+
+      const values = metric?.units?.USD || [];
+
+      // removeing duplicates values from raw data 
+      const unique = Object.values(
+        values.reduce((acc: any, item: any) => {
+          acc[item.end] = item;
+          return acc;
+        }, {})
+      );
+
+      // sort by date
+      const sorted = unique.sort(
+        (a: any, b: any) =>
+          new Date(a.end).getTime() - new Date(b.end).getTime()
+      );
+
+      // format for UI
+      return sorted.map((item: any) => ({
+        date: item.end,
+        value: item.val,
+        form: item.form,
+        period: item.fp,
+      }));
+    };
+
+    const latest = (arr: any[]) =>
+      arr.length ? arr[arr.length - 1] : null;
+
+
+    const revenue = cleanMetric(
+      facts?.ContractWithCustomerLiabilityRevenueRecognized
+    );
+
+    const assets = cleanMetric(facts?.Assets);
+    const liabilities = cleanMetric(facts?.Liabilities);
+
 
     return {
-      facts,
       companyName,
       revenue,
       liabilities,
-      assets  
+      assets ,
+      latest: {
+        revenue: latest(revenue),
+        assets: latest(assets),
+        liabilities: latest(liabilities),
+      },
     }
   } catch (err) {
     console.error(err);
