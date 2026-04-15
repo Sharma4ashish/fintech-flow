b1fac4e14264d239921cae096f4db433db8d7d9a
Ashish Sharma <90973051+Sharma4ashish@users.noreply.github.com>
2026-04-15 16:56:45 +0530

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,12 @@ export default function Home() {
       <h1 className="text-3xl font-bold  text-amber-200 underline">
       Hello world!
       </h1>
+      <div className="p-6">
+      <h1 className="text-xl  text-amber-200 mb-4">Financial Data Explorer</h1>
+        <div className="mt-4">
+          <h2 className="text-lg text-amber-200 font-semibold">Revenue Data</h2>
+        </div>
+    </div>
     </div>
   );
 }
